55ed6afc3d642297dd66edac164c095c0d7d42c9
root <mantillajulian234@gmail.com>
2024-10-18 20:52:27 -0500

    se agrega script run_flask_server.sh y corre servidor super basico

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print('hello world')
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def root():
+    current_directory = os.path.dirname(os.path.abspath(__file__))
+    last_directory = os.path.basename(current_directory)
+
+    return f'Current Project: {last_directory}'
---
+++ run_flask_server.sh
@@ -0,0 +1,6 @@
+export FLASK_APP=main.py
+export FLASK_DEBUG=1
+
+export FLASK_ENV=development
+echo $FLASK_ENV
+flask run
